9d4885727ecf09d194c177bf25f6003ef0e49eab
shabdm <shabd@sweepsouth.com>
2022-08-02 18:38:25 +0200

    implemenetd logging

--- authours_api/settings/base.py
+++ authours_api/settings/base.py
@@ -160,3 +160,25 @@ CORS_URLS_REGEX = r"^/api/.*$"  # cors is only added to endpoints that start wit
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# setting up logging
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
