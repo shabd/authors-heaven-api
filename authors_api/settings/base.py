cb86957967d72f39e8e233686055ed71ee7815ae
Shabd Maharaj <shabd@Shabds-MacBook-Pro.local>
2023-01-17 23:03:43 +0200

    created a customer user models and added swagger

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -154,6 +154,7 @@ MEDIA_ROOT = str(ROOT_DIR / "mediafiles")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_URLS_REGEX = r"^/api/.*$"
 
+AUTH_USER_MODEL = 'users.User'
 
 LOGGING = {
     "version": 1,
