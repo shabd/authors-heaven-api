4215be63dc1bf5fe9f595e45c130f4b3656ffc56
Shabd Maharaj <shabd@Shabds-MacBook-Pro.local>
2023-01-17 14:52:41 +0200

    added apps

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -39,7 +39,7 @@ THIRD_PARTY_APPS = [
     "corsheaders",
 ]
 
-LOCAL_APPS =[]
+LOCAL_APPS =["core_apps.common", "core_apps.profiles", "core_apps.users"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
