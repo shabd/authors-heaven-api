e8c79adcfd3c87b5251414576a3863c6b6d78ddf
Shabd Maharaj <shabd@Shabds-MacBook-Pro.local>
2023-01-18 14:56:18 +0200

    created user profile

--- core_apps/common/models.py
+++ core_apps/common/models.py
@@ -1,3 +1,13 @@
+import uuid 
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedUUIDModel(models.Model):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at","-updated_at"]
